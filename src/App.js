03c943a0894c5c313aec8f7787aa869ffcb2bb02
Patricia Jurado De Bilbao <patriciajdebilbao@gmail.com>
2020-06-17 19:51:26 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import React, { useState } from "react";
+import keys from "./keys";
 import './App.css';
 
+const api = {
+  key: keys.API_KEY,
+  base: keys.BASE_URL,
+  icon_base: keys.ICON_BASE_URL
+};
+
 function App() {
+  const dateBuild = (d) => {
+    let date = String(new window.Date());
+    date = date.slice(3, 15);
+    return date;
+  };
+
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const search = (e) => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setQuery("");
+          setWeather(result);
+          console.log(result);
+        });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 18
+            ? "App hot"
+            : "App cold"
+          : "App"
+      }
+    >
+      <main>
+
+      {typeof weather.main === "undefined" ? (
+        <article>
+            <h1 className="title"> My Weather App</h1>
+            <h3 className="text">What's the weather in...?</h3>
+        </article>
+        ) : (
+          ''
+        )}
+
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="search-bar"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+
+
+       
+
+        {typeof weather.main != "undefined" ? (
+
+          <div>
+
+            <div className="location-container">
+              <div className="date"> 
+                Today, {dateBuild(new Date())}
+              </div>
+
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+            </div>
+
+            <div className="weather-container">
+              <div className="weather"> 
+                <img src={`${api.icon_base}${weather.weather[0].icon}@4x.png`} alt={`${weather.weather[0].main}`} />
+              </div>
+
+              <div className="temperature">
+                {Math.round(weather.main.temp)}°
+              </div>
+
+              <div className="weather-type">
+                {weather.weather[0].description}
+              </div>
+
+            <div>
+              <div className="weather-feels">
+                Feels like {Math.round(weather.main.feels_like)}º
+              </div>
+
+              <div className="humidity">
+                  Humidity {weather.main.humidity}%
+              </div>
+            </div>
+
+            </div>
+
+          </div>
+
+        ) : (
+          ''
+        )}
+      </main>
     </div>
   );
 }
---
+++ src/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    API_KEY: "826da1bab591704b17d0eb64c374a81e",
+    BASE_URL: "https://api.openweathermap.org/data/2.5/",
+    ICON_BASE_URL: "http://openweathermap.org/img/wn/"
+  }
