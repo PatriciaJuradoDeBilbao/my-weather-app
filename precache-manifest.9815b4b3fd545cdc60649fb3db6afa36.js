c125882148fc007f1fe30fdfefdfbdafc14b33df
Patricia Jurado De Bilbao <patriciajdebilbao@gmail.com>
2020-06-17 20:01:27 +0200

    Updates

---
+++ precache-manifest.9815b4b3fd545cdc60649fb3db6afa36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c660cefb74d55fac36cdf347041c416e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bf3a8235efc49639d56",
+    "url": "/static/css/main.83af3386.chunk.css"
+  },
+  {
+    "revision": "11d7ff3e7de2fd507702",
+    "url": "/static/js/2.bb005d7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bb005d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bf3a8235efc49639d56",
+    "url": "/static/js/main.83d19e19.chunk.js"
+  },
+  {
+    "revision": "61a341f5efba173836f0",
+    "url": "/static/js/runtime-main.ad05990a.js"
+  },
+  {
+    "revision": "50527bbcbc7093b42592c2525e0564df",
+    "url": "/static/media/bg.50527bbc.jpg"
+  }
+]);
