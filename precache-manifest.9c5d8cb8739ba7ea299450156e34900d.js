e19f636eeb512940fc5c47f2258580e1bdb81058
Patricia Jurado De Bilbao <patriciajdebilbao@gmail.com>
2020-06-17 20:10:27 +0200

    Updates

---
+++ precache-manifest.9c5d8cb8739ba7ea299450156e34900d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690da0509b5bb9522633f12fc36a1c82",
+    "url": "/my-weather-app/index.html"
+  },
+  {
+    "revision": "22a585c4aef8f9b4b854",
+    "url": "/my-weather-app/static/css/main.51f3febe.chunk.css"
+  },
+  {
+    "revision": "11d7ff3e7de2fd507702",
+    "url": "/my-weather-app/static/js/2.bb005d7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-weather-app/static/js/2.bb005d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22a585c4aef8f9b4b854",
+    "url": "/my-weather-app/static/js/main.be0a396a.chunk.js"
+  },
+  {
+    "revision": "320153617ae488acd0f8",
+    "url": "/my-weather-app/static/js/runtime-main.9bd7dffe.js"
+  },
+  {
+    "revision": "50527bbcbc7093b42592c2525e0564df",
+    "url": "/my-weather-app/static/media/bg.50527bbc.jpg"
+  }
+]);
